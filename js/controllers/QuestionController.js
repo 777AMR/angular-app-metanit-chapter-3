dc524412f783f5632b9c7e663ba83385360dc330
777AMR <meyram.ramazanov@gmail.com>
2022-10-29 15:44:16 +0600

    Сервис $http и ajax-запросы

--- js/controllers/QuestionController.js
+++ js/controllers/QuestionController.js
@@ -1,8 +1,14 @@
 questApp.controller('QuestionController',
-    function QuestionController($scope, questionService) {
+    function QuestionController($scope, $http) {
 
-        $scope.question = questionService.question;
+        $scope.loaded = false;
 
+        $scope.load = function () {
+            $http({method: 'GET', url: 'question.json'}).then(function success(response) {
+                $scope.question = response.data.question;
+                $scope.loaded = true;
+            });
+        };
         $scope.voteUp = function (answer) {
             answer.rate++;
         };
